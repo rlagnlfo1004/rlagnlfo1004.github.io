eef033718baa188390bed8d13f253d8360edaa56
김휘래 <hramst0618@gmail.com>
2026-08-06 23:41:53 +0900

    도메인을 it.looksgood2.me 로 변경하고 부제 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 // 커스텀 도메인. public/CNAME 파일과 값이 같아야 한다.
 export default defineConfig({
-  site: 'https://blog.looksgood2.me',
+  site: 'https://it.looksgood2.me',
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
